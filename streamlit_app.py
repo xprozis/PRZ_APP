1dd33c4a42a851847940bf1bb3e24491315323c7
joaoantonio <joaoantonio.ferreira@prozis.com>
2024-11-28 16:36:15 +0000

    Data_reader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import pandas as pd
 
-st.title("PROZIS DATA LOG")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="PROZIS",
+    page_icon="🔴",
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
+
+st.title("Warehouse Dashboard")
+
+
+
+def read_excel_file():
+
+    # Exemplo de uma BOM (EXEMPLO
+    BOM_SD = "./Prozis Group S.A/ElectronicX - Hardware/Projects/pcb0002 Prozis Smart Device/pcb0002HR - Heart Rate Board/V2.2/pcb0002HR_v2_2/Production/Assembly/"
+
+    # read by default 1st sheet of an excel file
+    dataframe1 = pd.read_excel(BOM_SD + "pcb0002HR_v2_2_bom.xlsx")
+    print("testez")
+
+read_excel_file()
