0a99db2fd5b5c057f011634dfb2d1552b80292dd
anamemartins <ana.emartins@prozis.com>
2024-12-12 15:49:02 +0000

    dsds

--- pages/3_❗_Help.py
+++ pages/3_❗_Help.py
@@ -6,6 +6,7 @@ st.set_page_config(
 )
 
 
+
 st.write("# Welcome to our Help page! 👋")
 image_path = "https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80"
 image_path_2 = "./pages/shared/Logo_Prozis.png"
